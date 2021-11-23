61e163f405985a5f959714a231e9c2b064e1beee
Nils Werner <nils.werner@audiolabs-erlangen.de>
2021-11-23 13:50:45 +0100

    Deprecate arguments: in favor of _args:

--- crestic.py
+++ crestic.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import argparse
+import warnings
 import subprocess
 import configparser
 
@@ -67,6 +68,8 @@ def pathexpand(val):
 
 
 def main(argv, environ=None, conffile=None, dryrun=None, executable=None):
+    warnings.simplefilter(action='always', category=DeprecationWarning)
+
     if environ is None:
         environ = os.environ
 
@@ -146,14 +149,21 @@ def main(argv, environ=None, conffile=None, dryrun=None, executable=None):
         for k, v in restic_options.items()
     }
 
+    try:
+        restic_options["_args"] = restic_options['arguments']
+        del restic_options['arguments']
+        warnings.warn("The use of the arguments: key is deprecated, please use _args: instead.", DeprecationWarning)
+    except KeyError:
+        pass
+
     # Override config arguments with arguments from CLI
     if python_args.arguments:
-        restic_options['arguments'] = python_args.arguments
+        restic_options['_args'] = python_args.arguments
 
     # Extract positional arguments from options dict
     try:
-        restic_arguments = restic_options['arguments']
-        del restic_options['arguments']
+        restic_arguments = restic_options['_args']
+        del restic_options['_args']
     except KeyError:
         restic_arguments = []
 
